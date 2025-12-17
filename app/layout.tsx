468fa230e1c753c053945b3b97d5c1b317b09f3b
harlequinetcie <ezequiel.cura@gmail.com>
2025-12-17 16:25:16 +0100

    added github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,6 @@ export default function RootLayout({
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src="images/linear-light-logo.svg" alt="Linear Logo" className="h-5 w-5" />
-              <span>Linear</span>
             </a>
             <a
               href="https://github.com/desplega-ai/github-issues-swipe/issues/2"
@@ -51,7 +50,16 @@ export default function RootLayout({
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src="images/jira.png" alt="Jira Logo" className="h-5 w-5" />
-              <span>Jira</span>
+            </a>
+            <a
+              href="https://github.com/desplega-ai/github-issues-swipe"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 rounded-lg border-2 border-gray-700 bg-gray-900/90 px-4 py-2 text-sm font-medium text-gray-300 backdrop-blur-sm transition-all hover:border-blue-500 hover:text-blue-400 hover:shadow-lg"
+              title="View on GitHub"
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="images/github-mark-white.png" alt="GitHub Logo" className="h-5 w-5" />
             </a>
           </div>
         </div>
